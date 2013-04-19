d46c7b86babcac71dbd3ee415acaeea8dfac33b1
David Grant <david@grant.org.uk>
2013-04-19 20:50:38 +0100

    Added simpleEnroll (#2) and simpleReEnroll (#3).
    Response status of 200 is now covered.  Need to add branch for status
    of 202.

--- jester-client/src/main/java/org/jscep/jester/EstClient.java
+++ jester-client/src/main/java/org/jscep/jester/EstClient.java
@@ -1,50 +1,109 @@
 package org.jscep.jester;
 
 import org.apache.commons.codec.binary.Base64InputStream;
+import org.apache.commons.codec.binary.Base64OutputStream;
 import org.apache.http.*;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
 import org.jscep.jester.io.EntityDecoder;
+import org.jscep.jester.io.EntityEncoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.security.KeyStore;
 import java.security.cert.X509Certificate;
+import java.util.Arrays;
 
 public class EstClient {
     private static final Logger LOGGER = LoggerFactory.getLogger(EstClient.class);
     public static final String WELL_KNOWN_LOCATION = "/.well-known/est";
+    private static final String CA_CERTIFICATES_DISTRIBUTION = "/CACerts";
+    private static final String SIMPLE_ENROLL = "/simpleEnroll";
+    private static final String SIMPLE_RENEW = "/simpleReEnroll";
     private String host;
     private HttpClient httpClient;
     private EntityDecoder<X509Certificate[]> certDecoder;
+    private EntityEncoder<CertificationRequest> csrEncoder;
+    private final KeyStore trustStore;
 
-    public EstClient(HttpClient httpClient, EntityDecoder<X509Certificate[]> certDecoder, String host) {
+    public EstClient(HttpClient httpClient, EntityDecoder<X509Certificate[]> certDecoder, EntityEncoder<CertificationRequest> csrEncoder, String host) {
         this.host = host;
         this.certDecoder = certDecoder;
+        this.csrEncoder = csrEncoder;
         this.httpClient = httpClient;
+        this.trustStore = null;
+    }
+
+    public EstClient(HttpClient httpClient, EntityDecoder<X509Certificate[]> certDecoder, EntityEncoder<CertificationRequest> csrEncoder, String host, KeyStore explicitCaDatabase) {
+        this.host = host;
+        this.certDecoder = certDecoder;
+        this.csrEncoder = csrEncoder;
+        this.httpClient = httpClient;
+        this.trustStore = explicitCaDatabase;
     }
 
     public X509Certificate[] obtainCaCertificates() throws IOException, EstProtocolException {
-        HttpGet get = new HttpGet("http://" + host + WELL_KNOWN_LOCATION + "/CACerts");
+        HttpGet get = new HttpGet("https://" + host + WELL_KNOWN_LOCATION + CA_CERTIFICATES_DISTRIBUTION);
         HttpResponse response = httpClient.execute(get);
 
-        if (response.getStatusLine().getStatusCode() != 200) {
-            throw new EstProtocolException("Status code should be 200");
-        }
-
-        Header contentType = response.getFirstHeader(HttpHeaders.CONTENT_TYPE);
-        if (contentType == null || !contentType.getValue().equals("application/pkcs7-mime")) {
-            throw new EstProtocolException("Content type should be application/pkcs7-mime");
-        }
-
-        Header contentTransferEncoding = response.getFirstHeader("Content-Transfer-Encoding");
-        if (contentTransferEncoding == null || !contentTransferEncoding.getValue().equals("base64")) {
-            throw new EstProtocolException("Content transfer encoding should be base64");
-        }
+        int statusCode = response.getStatusLine().getStatusCode();
+        checkStatusCode(statusCode, 200);
+        checkContentType(response);
+        checkContentTransferEncoding(response);
 
         HttpEntity entity = response.getEntity();
         X509Certificate[] certs = certDecoder.decode(new Base64InputStream(entity.getContent()));
 
         return certs;
+    }
+
+    private void checkStatusCode(int actual, int... expected) throws EstProtocolException {
+        if (Arrays.binarySearch(expected, actual) > 0) {
+            throw new EstProtocolException("Status code should be in " + Arrays.toString(expected));
+        }
+    }
+
+    private void checkContentTransferEncoding(HttpResponse response) throws EstProtocolException {
+        Header contentTransferEncoding = response.getFirstHeader("Content-Transfer-Encoding");
+        if (contentTransferEncoding == null || !contentTransferEncoding.getValue().equals("base64")) {
+            throw new EstProtocolException("Content transfer encoding should be base64");
+        }
+    }
+
+    public X509Certificate enroll(CertificationRequest csr) throws IOException {
+        return enroll(csr, SIMPLE_ENROLL);
+    }
+
+    public X509Certificate renew(CertificationRequest csr) throws IOException {
+        return enroll(csr, SIMPLE_RENEW);
+    }
+
+    private X509Certificate enroll(CertificationRequest csr, String command) throws IOException {
+        HttpPost post = new HttpPost("https://" + host + WELL_KNOWN_LOCATION + command);
+        ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+        csrEncoder.encode(csr, new Base64OutputStream(bOut));
+        post.setEntity(new ByteArrayEntity(bOut.toByteArray()));
+        HttpResponse response = httpClient.execute(post);
+
+        int statusCode = response.getStatusLine().getStatusCode();
+        checkStatusCode(statusCode, 200, 202);
+        checkContentType(response);
+        checkContentTransferEncoding(response);
+
+        HttpEntity entity = response.getEntity();
+        X509Certificate[] certs = certDecoder.decode(new Base64InputStream(entity.getContent()));
+
+        return certs[0];
+    }
+
+    private void checkContentType(HttpResponse response) throws EstProtocolException {
+        Header contentType = response.getFirstHeader(HttpHeaders.CONTENT_TYPE);
+        if (contentType == null || !contentType.getValue().equals("application/pkcs7-mime")) {
+            throw new EstProtocolException("Content type should be application/pkcs7-mime");
+        }
     }
 }
--- jester-client/src/test/java/org/jscep/jester/EstClientTest.java
+++ jester-client/src/test/java/org/jscep/jester/EstClientTest.java
@@ -5,63 +5,79 @@ import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.message.BasicHeader;
 import org.jscep.jester.io.EntityDecoder;
+import org.jscep.jester.io.EntityEncoder;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+
 import static org.junit.Assert.*;
 
-import java.io.IOException;
 import java.io.InputStream;
+import java.security.KeyStore;
 import java.security.cert.X509Certificate;
+import java.util.Arrays;
 
 import static org.mockito.Mockito.*;
 
 public class EstClientTest {
 
     private EstClient estClient;
-    private X509Certificate[] expected;
+    private X509Certificate[] expectedCerts;
     private HttpClient httpClient;
     private HttpResponse httpResponse;
     private HttpEntity httpEntity;
     private EntityDecoder<X509Certificate[]> certDecoder;
+    private EntityEncoder<CertificationRequest> csrEncoder;
     private StatusLine statusLine;
+    private X509Certificate expectedCert;
 
     @Before
     public void setUp() throws Exception {
-        expected = new X509Certificate[0];
+        expectedCert = mock(X509Certificate.class);
+        expectedCerts = new X509Certificate[] {expectedCert};
         certDecoder = mock(EntityDecoder.class);
+        csrEncoder = mock(EntityEncoder.class);
         httpClient = mock(HttpClient.class);
         httpResponse = mock(HttpResponse.class);
         httpEntity = mock(HttpEntity.class);
         statusLine = mock(StatusLine.class);
-        estClient = new EstClient(httpClient, certDecoder, "www.example.com");
+        estClient = new EstClient(httpClient, certDecoder, csrEncoder, "www.example.com");
 
-        when(certDecoder.decode(any(InputStream.class))).thenReturn(expected);
+        when(certDecoder.decode(any(InputStream.class))).thenReturn(expectedCerts);
         when(httpResponse.getStatusLine()).thenReturn(statusLine);
         when(httpResponse.getEntity()).thenReturn(httpEntity);
         when(httpClient.execute(any(HttpUriRequest.class))).thenReturn(httpResponse);
     }
 
+    @Test
+    public void testSSL() throws Exception {
+        SSLContext ctx = SSLContext.getInstance("TLS");
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        tmf.init((KeyStore) null);
+        System.out.println(Arrays.toString(tmf.getTrustManagers()));
+    }
+
     @Test(expected = EstProtocolException.class)
     public void testIncorrectStatusCode() throws Exception {
-        assertArrayEquals(expected, estClient.obtainCaCertificates());
+        assertArrayEquals(expectedCerts, estClient.obtainCaCertificates());
     }
 
     @Test(expected = EstProtocolException.class)
     public void testInvalidContentType() throws Exception {
         when(statusLine.getStatusCode()).thenReturn(200);
-        assertArrayEquals(expected, estClient.obtainCaCertificates());
+        assertArrayEquals(expectedCerts, estClient.obtainCaCertificates());
     }
 
     @Test(expected = EstProtocolException.class)
     public void testInvalidContentTransferEncoding() throws Exception {
         when(statusLine.getStatusCode()).thenReturn(200);
         when(httpResponse.getFirstHeader(HttpHeaders.CONTENT_TYPE)).thenReturn(new BasicHeader(HttpHeaders.CONTENT_TYPE, "application/pkcs7-mime"));
-        assertArrayEquals(expected, estClient.obtainCaCertificates());
+        assertArrayEquals(expectedCerts, estClient.obtainCaCertificates());
     }
 
     @Test
@@ -70,6 +86,42 @@ public class EstClientTest {
         when(httpResponse.getFirstHeader(HttpHeaders.CONTENT_TYPE)).thenReturn(new BasicHeader(HttpHeaders.CONTENT_TYPE, "application/pkcs7-mime"));
         when(httpResponse.getFirstHeader("Content-Transfer-Encoding")).thenReturn(new BasicHeader("Content-Transfer-Encoding", "base64"));
 
-        assertArrayEquals(expected, estClient.obtainCaCertificates());
+        assertArrayEquals(expectedCerts, estClient.obtainCaCertificates());
+    }
+
+    @Test(expected = EstProtocolException.class)
+    public void testEnrollBadStatusCode() throws Exception {
+        CertificationRequest csr = mock(CertificationRequest.class);
+        when(statusLine.getStatusCode()).thenReturn(400);
+
+        estClient.enroll(csr);
+    }
+
+    @Test(expected = EstProtocolException.class)
+    public void testEnrollInvalidContentType() throws Exception {
+        CertificationRequest csr = mock(CertificationRequest.class);
+        when(statusLine.getStatusCode()).thenReturn(200);
+
+        estClient.enroll(csr);
+    }
+
+    @Test(expected = EstProtocolException.class)
+    public void testEnrollInvalidContentTransferEncoding() throws Exception {
+        CertificationRequest csr = mock(CertificationRequest.class);
+        when(statusLine.getStatusCode()).thenReturn(200);
+        when(httpResponse.getFirstHeader(HttpHeaders.CONTENT_TYPE)).thenReturn(new BasicHeader(HttpHeaders.CONTENT_TYPE, "application/pkcs7-mime"));
+
+        estClient.enroll(csr);
+    }
+
+    @Test
+    public void testEnroll() throws Exception {
+        CertificationRequest csr = mock(CertificationRequest.class);
+        when(statusLine.getStatusCode()).thenReturn(200);
+        when(httpResponse.getFirstHeader(HttpHeaders.CONTENT_TYPE)).thenReturn(new BasicHeader(HttpHeaders.CONTENT_TYPE, "application/pkcs7-mime"));
+        when(httpResponse.getFirstHeader("Content-Transfer-Encoding")).thenReturn(new BasicHeader("Content-Transfer-Encoding", "base64"));
+
+        X509Certificate actual = estClient.enroll(csr);
+        assertEquals(expectedCert, actual);
     }
 }
