f2508c85f96aa51e03503695f5809912b34862ab
David Grant <david@grant.org.uk>
2014-04-02 19:11:23 +0100

    Integrate with libest

--- jester-sample-war/src/test/java/org/jscep/jester/EstClientIT.java
+++ jester-sample-war/src/test/java/org/jscep/jester/EstClientIT.java
@@ -1,7 +1,12 @@
 package org.jscep.jester;
 
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.HttpClient;
-import org.apache.http.conn.ssl.AllowAllHostnameVerifier;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.conn.ssl.*;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.conn.ssl.StrictHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.SSLContextBuilder;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
@@ -12,23 +17,30 @@ import org.jscep.jester.io.BouncyCastleSignedDataDecoder;
 import org.jscep.jester.io.EntityEncoder;
 import org.junit.Test;
 
+import javax.net.ssl.SSLContext;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 import static org.junit.Assert.assertNotNull;
 
 public class EstClientIT {
     @Test
     public void testObtainCaCertificates() throws IOException, Exception {
-        HttpClient httpClient = getNewHttpClient();
+        HttpClient httpClient = httpClient();
         EstClient estClient = new EstClient(httpClient, new BouncyCastleSignedDataDecoder(), new EntityEncoder<CertificationRequest>() {
             @Override
             public void encode(OutputStream out, CertificationRequest... entity) throws IOException {
                out.write(entity[0].getBytes()); 
             }
-        }, "localhost:8443");
-        assertNotNull(estClient.obtainCaCertificates());
+        }, "ec2-54-204-91-178.compute-1.amazonaws.com:8443");
+        X509Certificate[] explicitTaDatabase = estClient.obtainCaCertificates(); // 4.1.1
         estClient.enroll(getCertificationRequest());
     }
     
@@ -46,11 +58,51 @@ public class EstClientIT {
         };
     }
 
-    private HttpClient getNewHttpClient() throws Exception {
-        SSLContextBuilder builder = new SSLContextBuilder();
-        builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
-        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(builder.build(), new AllowAllHostnameVerifier());
-        return HttpClients.custom().setSSLSocketFactory(sslsf).build();
+    private HttpClient httpClient() throws Exception {
+        return HttpClients
+                .custom()
+                .setSSLSocketFactory(sslConnectionSocketFactory())
+                .setDefaultRequestConfig(requestConfig())
+                .setDefaultCredentialsProvider(credentialsProvider())
+                .build();
+    }
 
+    private SSLConnectionSocketFactory sslConnectionSocketFactory() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+        return new SSLConnectionSocketFactory(sslContext(), new StrictHostnameVerifier());
+    }
+
+    private SSLContext sslContext() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+        return new SSLContextBuilder()
+            .useProtocol("TLSv1.2")
+            .loadTrustMaterial(null, new TrustStrategy() {
+                @Override
+                public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                    return true;
+                }
+            })
+            .build();
+    }
+
+    private RequestConfig requestConfig() {
+        return RequestConfig.custom()
+                .setSocketTimeout(30000)
+                .setConnectTimeout(30000)
+                .setConnectionRequestTimeout(30000)
+                .build();
+    }
+
+    private BasicCredentialsProvider credentialsProvider() {
+        BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(
+                new AuthScope("ec2-54-204-91-178.compute-1.amazonaws.com", 8443), 
+                new UsernamePasswordCredentials("estuser", "estpwd"));
+        return credentialsProvider;
+    }
+
+    private KeyStore getKeyStore() throws Exception {
+        KeyStore store = KeyStore.getInstance("JKS");
+        store.load(getClass().getResourceAsStream("/jester.jks"), "jester".toCharArray());
+        
+        return store;
     }
 }
